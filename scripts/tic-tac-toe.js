f2fa627c219b6420729a3331c7d2ca2f29e00207
cfcrawford <camilafcrawford@gmail.com>
2016-05-16 03:14:41 -0700

    Adds reset button and element styling

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -5,9 +5,11 @@
 window.onload = function() {
   for (var i = 0; i < squares.length; i++){
     squares[i].addEventListener('click', changeSquare);
-    //squares[i].addEventListener('click', xOrO);
   }
+  document.getElementById("button").addEventListener('click', clearSquares);
 }
+
+
 
 function changeSquare() {
   if (play == 1) {
@@ -17,10 +19,6 @@ function changeSquare() {
   this.style.backgroundColor = 'red';
   this.style.pointerEvents = 'none';
   play -= 1;
-  //var id = squares[i];
-  //return i;
-  //alert("It is" + squares[i]);
-  //winner();
   }
 
   else {
@@ -28,33 +26,53 @@ function changeSquare() {
     this.style.backgroundColor = 'blue';
     this.style.pointerEvents = 'none';
     play += 1;
-    //turn = "O";
-    //winner();
-
   }
+
   winnerX();
   winnerO();
 }
-//checkWin();
 
-/*function alertBox() {
-  if (document.getElementsByClassName("square").innerHTML = "X") {
-    alert("Hi!");
-  }
-}; */
+function clearSquares() {
+  document.getElementById("square1").innerHTML = "";
+  document.getElementById("square2").innerHTML = "";
+  document.getElementById("square3").innerHTML = "";
+  document.getElementById("square4").innerHTML = "";
+  document.getElementById("square5").innerHTML = "";
+  document.getElementById("square6").innerHTML = "";
+  document.getElementById("square7").innerHTML = "";
+  document.getElementById("square8").innerHTML = "";
+  document.getElementById("square9").innerHTML = "";
+
+  document.getElementById("square1").style.backgroundColor = '';
+  document.getElementById("square2").style.backgroundColor = '';
+  document.getElementById("square3").style.backgroundColor = '';
+  document.getElementById("square4").style.backgroundColor = '';
+  document.getElementById("square5").style.backgroundColor = '';
+  document.getElementById("square6").style.backgroundColor = '';
+  document.getElementById("square7").style.backgroundColor = '';
+  document.getElementById("square8").style.backgroundColor = '';
+  document.getElementById("square9").style.backgroundColor = '';
+
+}
 
 
 function winnerX() {
   if (checkForWinner("X")) {
-    alert("Hi!");
+    newImage = "url(http://cdn.meme.am/instances/66547988.jpg)";
+  document.getElementById('board').style.backgroundImage = newImage;
+  document.getElementById('h1').innerHTML = "X wins!";
   }
 }
 
 function winnerO() {
   if (checkForWinner("O")) {
-    alert("Whoa!!!");
+    newImage = "url(http://cdn.meme.am/instances/66547988.jpg)";
+  document.getElementById('board').style.backgroundImage = newImage;
+  document.getElementById('h1').innerHTML = "O wins!";
   }
 }
+
+
 
 function checkForWinner(move) {
   var result = false;
@@ -82,196 +100,5 @@ function checkRow(a, b, c, move) {
 //gives a box based on a number
 function getBox(number) {
   return document.getElementById("square" + number).innerText;
-  //alert("hi");
 }
 
-/*
-
-//gives a box based on a number
-function getSquare(number) {
-  return document.getElementById("square" + number).innerText;
-}
-
-
-function getWinner() {
-  if ((document.getElementById("square1").innerHTML = "X") && (document.getElementById("square2").innerHTML = "X") && (document.getElementById("square3").innerHTML = "X")) {
-    alert("X is the winner!");
-  }
-
-} */
-
-/*function checkRow(a, b, c) {
-  if (a === "X" && b === "X" && c === "X") {
-    return 1;
-  }
-  else if (a === "O" && b === "O" && c === "O") {
-    return -1;
-  }
-  else {
-    return 0;
-  }
-}
-
-function checkWin(squares[i]) {
-  if checkRow(squares[0], squares[1], squares[2]) = "X" {
-    alert("hi");
-  }
-}
-
-/* function changeSquare() {
-  if (play == 1) {
-  //console.log(this.id);
-  this.innerHTML = "X";
-  this.value = "X";
-  this.style.backgroundColor = 'red';
-  this.disabled = "true";
-  play -= 1;
-  document.getElementsByClassName("square").disabled = "true";
-
-  }
-
-  else {
-    this.innerHTML = "O";
-    this.value = "O";
-    this.style.backgroundColor = 'blue';
-    play += 1;
-
-  }
-}
-
-
-
-
-function getWinner(number) {
-  var result = false;
-  if (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-    (getSquare === 4 && getSquare === 5 $$ getSquare === 6) ||
-    (getSquare === 7 && getSquare === 8 $$ getSquare === 9) ||
-    (getSquare === 1 && getSquare === 4 $$ getSquare === 3) ||
-    (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-    (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-    (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-      {
-        var result = true;
-    }
-    else result = false;
-}
-
-
-
-
-
-
-
-
-
-
-This function changes the color of a single square when you click on it more than once:
-
-
-function changeSquare() {
-
-  //console.log(this.id);
-  this.innerHTML = "X"
-  this.style.backgroundColor = this.style.backgroundColor=='red'? 'blue':'red';
-
-}
-
-But it's not what I'm trying to do
-
-
-
-var button = document.getElementById("your-button-id")
-window.onload = start;;
-button.onclick = function(){
-  //do stuff
-}
-  alert('Welcome to Tic Tac Toe');
-
-.onclick = changeColor;
-
-    function makeXAppear() {
-       var text = document.getElementById('one');
-       text.value = "X";
-    }*/
-/*
-var event = new customEvent()
-var square = document.getElementById('one');
-
-    function makeXAppear() {
-       document.getElementById('one');
-       if text.value =
-       text.value = "X";
-    }
-
-
-var count=1;
-function changeColor(button, color) {
-  var property = document.getElementById(button);
-       if (count == 1) {
-          property.style.backgroundColor = "#FFFFFF"
-          count=1;
-       }
-       else {
-        property.style.backgroundColor = "#7FFF00"
-        count = 0;
-       }
-    }
-*/
-/*
-
-var boxes = document.getElementsByTagName("td");
-var turnText = document.querySelector(".playerTurn");
-var counter = 1;
-var winCounter = 0;
-var OMoves = [];
-var XMoves = [];
-
-function start(){
-  addXandOListener();
-  addResetListener();
-}
-
-var function = xOrO() {
-  for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].addEventListener("click", addXorO);
-  };
-}
-
-
-var function = changeColor() {
-  for (var i = boxes.length - 1; i >= 0; i--) {
-   boxes[i].backgroundColor = #7FFF00;
-  }
-
-}
-*/
-
-
-
-/* function addEventListener(){
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("click", changeColor);
-  }
-}
-
-function changeColor(square) {
-    counter = 0;
-   for (var i = 0; i < squares.length; i++){
-    if ((counter % 2) === 0) {
-      square.style.background = 'red';
-      square.innerHTML = "X";
-      counter += 1;
-    }
-    else {
-      square.style.background = 'blue';
-      square.innerHTML = "O";
-    }
-  }
-}
-
-
-function resetGrid(squares) {
-  boxes.style.background = 'white';
-  boxes.innerHTML = " "
-}*/
