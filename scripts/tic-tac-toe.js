82dcaebc1fd6169cddca7ee84b28c431690e98c4
cfcrawford <camilafcrawford@gmail.com>
2016-05-15 21:30:02 -0700

    Fixes issue of changing letter in box multiple times

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,164 +1,40 @@
   var play = 1;
   var squares = document.getElementsByClassName("square");
+  var turn = "";
 
 window.onload = function() {
   for (var i = 0; i < squares.length; i++){
     squares[i].addEventListener('click', changeSquare);
+    //squares[i].addEventListener('click', xOrO);
   }
 }
 
+//function xOrO() {}
+
 
 function changeSquare() {
-  if (play % 2 != 0) {
+  if (play == 1) {
   //console.log(this.id);
   this.innerHTML = "X";
+  //document.getElementsByClassName("square").innerHTML = "X"
   this.style.backgroundColor = 'red';
+  this.style.pointerEvents = 'none';
   play -= 1;
-  document.getElementsByClassName("square").disabled = "true";
-
+  //return i;
+  //alert("It is" + i);
+  winner();
   }
 
   else {
     this.innerHTML = "O";
+    this.value = "O";
     this.style.backgroundColor = 'blue';
+    this.style.pointerEvents = 'none';
     play += 1;
+    //turn = "O";
+    winner();
+
   }
+  //return turn
 }
-
-function getSquare(number) {
-  return document.getElementById("square" + number).innerText;
-  alert("this workds")
-}
-
-function getWinner() {
-  if {([1, 2, 3] || [4,5,6] || [7,8,9]) ||
-    ([1, 4, 7] || [2, 5, 8] || [3, 5, 9]) ||
-    ([1, 5, 9] || [3, 5, 7])
-    alert("You are the winner!")
-  }
-  else {
-    alert("No winner found.")
-  }
-};
-
-
-/*function getWinner() {
-  if (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-    (getSquare === 4 && getSquare === 5 $$ getSquare === 6) ||
-    (getSquare === 7 && getSquare === 8 $$ getSquare === 9) ||
-    (getSquare === 1 && getSquare === 4 $$ getSquare === 3) ||
-    (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-    (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-    (getSquare === 1 && getSquare === 2 $$ getSquare === 3) ||
-}
-
-
-This function changes the color of a single square when you click on it more than once:
-
-
-function changeSquare() {
-
-  //console.log(this.id);
-  this.innerHTML = "X"
-  this.style.backgroundColor = this.style.backgroundColor=='red'? 'blue':'red';
-
-}
-
-But it's not what I'm trying to do
-
-
-
-var button = document.getElementById("your-button-id")
-window.onload = start;;
-button.onclick = function(){
-  //do stuff
-}
-  alert('Welcome to Tic Tac Toe');
-
-.onclick = changeColor;
-
-    function makeXAppear() {
-       var text = document.getElementById('one');
-       text.value = "X";
-    }*/
-
-/* var event = new customEvent()
-var square = document.getElementById('one');
-
-    function makeXAppear() {
-       document.getElementById('one');
-       if text.value =
-       text.value = "X";
-    }
-
-
-var count=1;
-function changeColor(button, color) {
-  var property = document.getElementById(button);
-       if (count == 1) {
-          property.style.backgroundColor = "#FFFFFF"
-          count=1;
-       }
-       else {
-        property.style.backgroundColor = "#7FFF00"
-        count = 0;
-       }
-    }
-*/
-/*
-
-var boxes = document.getElementsByTagName("td");
-var turnText = document.querySelector(".playerTurn");
-var counter = 1;
-var winCounter = 0;
-var OMoves = [];
-var XMoves = [];
-
-function start(){
-  addXandOListener();
-  addResetListener();
-}
-
-var function = xOrO() {
-  for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].addEventListener("click", addXorO);
-  };
-}
-
-
-var function = changeColor() {
-  for (var i = boxes.length - 1; i >= 0; i--) {
-   boxes[i].backgroundColor = #7FFF00;
-  }
-
-}
-*/
-
-
-
-/* function addEventListener(){
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("click", changeColor);
-  }
-}
-
-function changeColor(square) {
-    counter = 0;
-   for (var i = 0; i < squares.length; i++){
-    if ((counter % 2) === 0) {
-      square.style.background = 'red';
-      square.innerHTML = "X";
-      counter += 1;
-    }
-    else {
-      square.style.background = 'blue';
-      square.innerHTML = "O";
-    }
-  }
-}
-
-
-function resetGrid(squares) {
-  boxes.style.background = 'white';
-  boxes.innerHTML = " "
-}*/
+checkWin();
